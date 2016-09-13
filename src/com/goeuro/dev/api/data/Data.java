a505c33a49829ddedad279cbd32601dd73fd1a56
DraganVeljovic <dragan.d.veljovic@gmail.com>
2016-09-14 01:06:34 +0200

    Change getTypes to getMetadata.

--- src/com/goeuro/dev/api/data/Data.java
+++ src/com/goeuro/dev/api/data/Data.java
@@ -5,5 +5,5 @@ import com.google.gson.JsonObject;
 public interface Data {
 	public void setValues(JsonObject obj);
 	public String[] getValues();
-	public String[] getTypes();
+	public String[] getMetada();
 }
