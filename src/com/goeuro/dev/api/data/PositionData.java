e085874b5143c91aef8def7ef76fd724cd17cff2
DraganVeljovic <dragan.d.veljovic@gmail.com>
2016-09-14 01:06:58 +0200

    Add type field.

--- src/com/goeuro/dev/api/data/PositionData.java
+++ src/com/goeuro/dev/api/data/PositionData.java
@@ -5,18 +5,21 @@ import com.google.gson.JsonObject;
 public class PositionData implements Data {
 	private static final String ID = "_id";
 	private static final String NAME = "name";
+	private static final String TYPE = "type";
 	private static final String GEO_POSITION = "geo_position";
 	private static final String LATITUDE = "latitude";
 	private static final String LONGITUDE = "longitude";
 	
 	private int id;
 	private String name;
+	private String type;
 	private double latitude;
 	private double longitude;
 	
 	public void setValues(JsonObject obj) {
 		id = obj.get(ID).getAsInt();
 		name = obj.get(NAME).getAsString();
+		type = obj.get(TYPE).getAsString();
 		
 		JsonObject geoPosition = obj.get(GEO_POSITION).getAsJsonObject();
 		latitude = geoPosition.get(LATITUDE).getAsDouble();
@@ -24,24 +27,26 @@ public class PositionData implements Data {
 	}
 	
 	public String[] getValues() {
-		String[] values = new String[4];
+		String[] values = new String[5];
 		
 		values[0] = Integer.toString(id);
 		values[1] = name;
-		values[2] = Double.toString(latitude);
-		values[3] = Double.toString(longitude);
+		values[2] = type;
+		values[3] = Double.toString(latitude);
+		values[4] = Double.toString(longitude);
 		
 		return values;
 	}
 	
-	public String[] getTypes() {
-		String[] types = new String[4];
+	public String[] getMetada() {
+		String[] metadata = new String[5];
 		
-		types[0] = ID;
-		types[1] = NAME;
-		types[2] = LATITUDE;
-		types[3] = LONGITUDE;
+		metadata[0] = ID;
+		metadata[1] = NAME;
+		metadata[2] = TYPE;
+		metadata[3] = LATITUDE;
+		metadata[4] = LONGITUDE;
 		
-		return types;
+		return metadata;
 	}
 }
