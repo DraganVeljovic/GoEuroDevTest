38f4414f35000ceb06cbcf11a1559dd73f5e3d1e
DraganVeljovic <dragan.d.veljovic@gmail.com>
2016-09-13 23:17:01 +0200

    Update test.sh.

--- test.sh
+++ test.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-JAR_NAME="go-euro-dev-test.jar"
+JAR_NAME="dist/lib/GoEuroDevTest.jar"
 MAIN_CLASS="com.goeuro.dev.GoEuroTest"
 
-# Create .jar
-jar cfe $JAR_NAME $MAIN_CLASS -C bin/ .
+# Clean & Build application
+ant clean && ant
 
-# Run Test Cases
-echo "No args ..."
-java -cp libs/gson-2.3.1.jar:libs/opencsv-3.8.jar:$JAR_NAME $MAIN_CLASS
 echo
-echo "To many args ..."
-java -cp libs/gson-2.3.1.jar:libs/opencsv-3.8.jar:$JAR_NAME $MAIN_CLASS "1" "2" "3"
 echo
-echo "No output file specified ..."
-java -cp libs/gson-2.3.1.jar:libs/opencsv-3.8.jar:$JAR_NAME $MAIN_CLASS "Berlin"
+echo "===== Run Tests ====="
 echo
-echo "Output file specified ..."
-java -cp libs/gson-2.3.1.jar:libs/opencsv-3.8.jar:$JAR_NAME $MAIN_CLASS "Paris" "output/paris-output.csv"
-echo
+
+# Function to run the application
+# $1 - message to display
+# $2, ... - application args
+function run() {
+  echo $1
+  shift
+  java -cp libs/gson-2.3.1.jar:libs/opencsv-3.8.jar:$JAR_NAME $MAIN_CLASS $@
+  echo
+}
+
+# Run several test cases
+run "No args ..."
+run "Too many args ..." "1" "2" "3"
+run "No output file specified (use default output file ./output.csv) ..." "Berlin"
+run "Output file specified ..." "Paris" "output/paris-output.csv"
+
 echo "Done."
