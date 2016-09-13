386b86334c03fd4cc35ff40f1bb199c2ace009ab
DraganVeljovic <dragan.d.veljovic@gmail.com>
2016-09-13 22:16:01 +0200

    Parse input args.

--- src/com/goeuro/dev/GoEuroTest.java
+++ src/com/goeuro/dev/GoEuroTest.java
@@ -1,33 +1,58 @@
 package com.goeuro.dev;
 
-import java.io.IOException;
+import java.util.HashMap;
 
 import com.goeuro.dev.api.Api;
 import com.goeuro.dev.api.data.PositionData;
 import com.goeuro.dev.csv.CsvUtil;
 import com.google.gson.JsonArray;
 
+
 public class GoEuroTest {
-	public static void main(String[] args) {
-		if (args.length == 0) {
-			System.out.println(
-					"GoEuroTest cityName "
-					+ "[ csvFilename = output.csv ] "
-					+ "[ csvLocation = current directory ]");
-			System.exit(1);
+	private static String usage() {
+		return "Usage: "
+				+ "GoEuroTest cityName "
+				+ "[ csvFile = output.csv ] ";
+	}
+	
+	private static final String CITY_NAME = "cityName";
+	private static final String OUTPUT_FILE = "outputFile";
+	private static HashMap<String, String> parseArgs(String[] args) throws IllegalArgumentException {
+		HashMap<String, String> parsedArgs = new HashMap<String, String>();
+		
+		if (args.length == 0 || args.length > 2) {
+			throw new IllegalArgumentException(usage());
 		}
 		
 		String cityName = args[0];
-		if (!cityName.matches("[A-Z][a-z]+")) {
-			System.out.println("Invalid city name.\nCity name must match following pattern '[A-Z][a-z]+'.");
+		if (!cityName.matches("^[a-zA-Z\b]+$")) {
+			throw new IllegalArgumentException(
+					"Invalid city name.\nCity name must match following pattern '^[a-zA-Z\b]+$'.");
+		}
+		parsedArgs.put(CITY_NAME, cityName);
 		
+		if (args.length > 1) {
+			String outputFile = args[1];
+			parsedArgs.put(OUTPUT_FILE, outputFile);
 		}
 		
-		Api api = new Api();
-		CsvUtil csvUtil = new CsvUtil();
-		
-		JsonArray cities = api.getPositionSuggestion(cityName);
-		
-		csvUtil.writeToCsv(cities, new PositionData(), "output.csv");
+		return parsedArgs;
+	}
+	
+	public static void main(String[] args) {
+		try {
+			HashMap<String, String> parsedArgs = parseArgs(args);
+			
+			Api api = new Api();
+			CsvUtil csvUtil = new CsvUtil();
+			
+			String cityName = parsedArgs.get(CITY_NAME);
+			JsonArray cities = api.getPositionSuggestion(cityName);
+			
+			String outputFile = parsedArgs.get(OUTPUT_FILE);
+			csvUtil.writeToCsv(cities, PositionData.class, outputFile == null ? "output.csv" : outputFile);
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
